95485b3e835dd614573ad5d38f44f093394db960
minji <>
2021-12-04 23:17:34 +0900

    initial commit
    backend 전반적인 공부를 위한 프로젝트 생성

---
+++ ecommerce/src/main/java/org/example/ecommerce/ecommerceApplication.java
@@ -0,0 +1,9 @@
+package org.example.ecommerce;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ecommerceApplication {
+}
